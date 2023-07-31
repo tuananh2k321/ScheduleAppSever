2b6d9e91542fd38ea97184f1d8865457f769a4a5
tuananh2k321 <haizzj123@gmail.com>
2023-07-31 21:01:33 +0700

    initial

---
+++ database/connection.php
@@ -0,0 +1,13 @@
+<?php
+$databaseHost = '127.0.0.1:3306';
+$databaseName = 'SCHEDULE_APP';
+$databaseUsername = 'root';
+$databasePassword = 'anhde0canem';
+
+try {
+	$dbConn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", 
+						$databaseUsername, $databasePassword);
+	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
---
+++ database/scripts.sql
@@ -0,0 +1,3 @@
+create database if not exists SCHEDULE_APP;
+
+use  SCHEDULE_APP;
