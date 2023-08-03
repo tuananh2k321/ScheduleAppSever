e5d235030f3be0b51792bc4f744a3586a64edc35
VoThanhThe <113089675+VoThanhThe@users.noreply.github.com>
2023-08-03 10:33:33 +0700

    add api schedule

--- database/scripts.sql
+++ database/scripts.sql
@@ -10,17 +10,23 @@ use  SCHEDULE_APP;
 -- user
 -- api: lưu user
 
+create table if not exists users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	avatar VARCHAR(150) NOT NULL,
+	name NVARCHAR(100) NOT NULL, -- tiếng việt
+	email VARCHAR(100) NOT NULL UNIQUE,
+    student_code VARCHAR(100) NOT NULL UNIQUE,
+    gender BIT,
+    birthday DATE,
+    address NVARCHAR(500),
+    course NVARCHAR(100)
+);
 
 
-create TABLE if not exists users(
-    studentCode VARCHAR(7) NOT NULL PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
-    avatar VARCHAR(1000) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    isLogged BIT DEFAULT 1
-)
-
+insert into users (avatar, name, email, student_code, gender, birthday, address, course) values ('', 'Võ Thành Thế', 'abc@gmail.com', 'PS24513', 1 , '2003-04-23', 'Hàm Thuận Bắc, Bình Thuận', 'Android Networking');
+insert into users (avatar, name, email, student_code, gender, birthday, address, course) values ('', 'Võ Chi Toàn', 'abc1@gmail.com', 'PS24514', 1 , '2003-04-23', 'Hàm Thuận Bắc, Bình Thuận', 'Android Networking');
+insert into users (avatar, name, email, student_code, gender, birthday, address, course) values ('', 'Võ Đài Nghĩa ', 'abc2@gmail.com', 'PS24515', 1 , '2003-04-23', 'Hàm Thuận Bắc, Bình Thuận', 'Android Networking');
+insert into users (avatar, name, email, student_code, gender, birthday, address, course) values ('', 'Võ Thị Thanh Tuyền', 'abc3@gmail.com', 'PS24516', 1 , '2003-04-23', 'Hàm Thuận Bắc, Bình Thuận', 'Android Networking');
 
 -- category
 -- api: get category
@@ -74,11 +80,33 @@ insert into news (id, title, image, detail, categoryID) values (18, "Học Phí"
 
 -- schedule
 -- api: get all schedule, get schedule by date
-create TABLE if not exists schedules(
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    date DATE NOT NULL,
+create table if not exists schedules (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    room NVARCHAR(100) NOT NULL, -- phòng
+    day DATE NOT NULL, -- ngày học
+    time NVARCHAR(100) NOT NULL, -- ca học
+    course_name NVARCHAR(100) NOT NULL, -- tên môn
+    course_id NVARCHAR(100) NOT NULL, -- mã môn
+    class_name NVARCHAR(100) NOT NULL, -- mã lớp
+    teacher_name NVARCHAR(100) NOT NULL, -- tên giảng viên
+    address NVARCHAR(100) NOT NULL, -- địa điểm
+    type BIT NOT NULL, -- 0: học, 1: thi
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
-)
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-01', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 1);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-02', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 0, 1);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-03', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 1);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-05', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 0, 1);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-06', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 2);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-07', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 2);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-08', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 0, 2);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-09', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 2);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-10', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 0, 3);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-11', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 0, 3);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-12', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 3);
+insert into schedules (room, day, time, course_name, course_id, class_name, teacher_name, address, type, user_id) values ('T308', '2023-08-12', '17:30 - 19:30 PM', 'Android Networking', 'MOB1234',  'MD17306', 'Chann3', 'Phan Mem Quang Trung', 1, 3);
 
 
 -- notification
